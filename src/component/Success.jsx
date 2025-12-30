0aa81ff95661df05287cf22ff00f5f30416e4dda
tktecno <68456825+tktecno@users.noreply.github.com>
2025-12-31 02:14:00 +0530

    Add files via upload

---
+++ src/component/Success.jsx
@@ -0,0 +1,9 @@
+export default function Success() {
+    return (
+      <div className="p-10 text-center">
+        <h1 className="text-3xl font-bold mb-4">Thanks!</h1>
+        <p>Your message has been sent. I’ll get back to you soon.</p>
+      </div>
+    );
+  }
+  
