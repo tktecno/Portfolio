4c162a0bea60b4f2117253e884e7544f5ed0bf58
tktecno <68456825+tktecno@users.noreply.github.com>
2025-12-31 02:13:03 +0530

    Add Success route to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Portfolio from "./component/Portfolio.jsx";
 import EduhubPreview from "./component/project/WorldEduhub.jsx";
-
+import Success from "./component/Success.jsx";
 
 export default function App() {
   return (
@@ -9,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Portfolio />} />
         <Route path="/world-eduhub" element={<EduhubPreview />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
     </Router>
   );
